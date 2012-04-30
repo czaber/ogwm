424083c2f016e1c3879f6f9092c35d8eda5e2ad5
Bartek Chaber <chaberb@iem.pw.edu.pl>
2012-04-30 22:30:52 +0200

    Missing files

---
+++ main.h
@@ -0,0 +1,110 @@
+#ifndef _UWM_H_
+#define _UWM_H_
+// Standard
+#include <string.h>
+#include <stdio.h>
+// Pure X11
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+// Extensions
+#include <X11/extensions/Xcomposite.h>
+#include <X11/extensions/Xfixes.h>
+#include <X11/extensions/shape.h>
+// OpenGL (GLX)
+#include <GL/glx.h>
+#include <GL/glxext.h>
+// Defines
+#define EVENTS_SIZE LASTEvent
+
+void print(Window w);
+void for_windows(Window w, void (*f)(Window w));
+void on_keypress(XEvent* e) {};
+void on_expose(XEvent* e) {};
+void on_maprequest(XEvent* e);
+void on_mapnotify(XEvent* e);
+void on_enternotify(XEvent* e) {};
+void on_unmapnotify(XEvent* e) {};
+void on_buttonpressed(XEvent* e) {};
+void on_destroynotify(XEvent* e);
+void on_visibilitynotify(XEvent* e) {};
+void on_createnotify(XEvent* e);
+void on_configurenotify(XEvent* e) {};
+void on_configurerequest(XEvent* e) {};
+Window create_overlay(void);
+Window create_backplane(void);
+Window create_frontplane(void);
+void ignore_input(Window w);
+void check_gl(int line);
+void setup_x(void);
+void setup_gl(void);
+void start(void);
+void clean(void);
+GLXFBConfig choose_fbconfig();
+
+typedef struct _Client {
+	Window window;
+	GLXPixmap glxpixmap;
+	GLenum target;
+	GLuint texture;
+} Client;
+
+// Events array
+static void (*events[EVENTS_SIZE])(XEvent* e) = {
+	[KeyPress] = on_keypress,
+	[Expose] = on_expose,
+	[MapRequest] = on_maprequest,
+	[MapNotify] = on_mapnotify,
+	[EnterNotify] = on_enternotify,
+	[UnmapNotify] = on_unmapnotify,
+	[ButtonPress] = on_buttonpressed,
+	[DestroyNotify] = on_destroynotify,
+	[VisibilityNotify] = on_visibilitynotify,
+	[CreateNotify] = on_createnotify,
+	[ConfigureNotify] = on_configurenotify,
+	[ConfigureRequest] = on_configurerequest
+};
+
+static char* event_names[EVENTS_SIZE] = {
+	//[KeyPress] = "KeyPress",
+	//[MotionNotify] = "MotionNotify",
+	[Expose] = "Expose",
+	//[FocusIn] = "FocusIn",
+	//[FocusOut] = "FocusOut",
+	[MapRequest] = "MapRequest",
+	[MapNotify] = "MapNotify",
+	[CreateNotify] = "CreateNotify",
+	//[EnterNotify] = "EnterNotify",
+	[UnmapNotify] = "UnmapNotify",
+	//[ButtonPress] = "ButtonPress",
+	//[ReparentNotify] = "ReparentNotify",
+	[DestroyNotify] = "DestroyNotify",
+	[VisibilityNotify] = "VisibilityNotify",
+	[ConfigureNotify] = "ConfigureNotify",
+	[ConfigureRequest] = "ConfigureRequest"
+};
+
+//static int gl_attr[] = { GLX_RGBA, GLX_DOUBLEBUFFER, None };
+//static int pixmap_attr[] = {GLX_TEXTURE_FORMAT_EXT, GLX_TEXTURE_FORMAT_RGBA_EXT, 
+//			    GLX_TEXTURE_TARGET_EXT, GLX_TEXTURE_RECTANGLE_EXT, None};
+static int gl_attr[] = {
+        GLX_DOUBLEBUFFER, True, // We want double-buffering
+        GLX_DEPTH_SIZE, 24, // Highest non-RGBA depth possible
+        GLX_RENDER_TYPE, GLX_RGBA_BIT, // Don't consider indexed color configs
+        GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT, // Must be a window config
+        GLX_X_RENDERABLE, True, // Only consider configs that have an associated X visual
+	None
+};
+/*static int pixmap_config[] = {
+	GLX_DRAWABLE_TYPE, GLX_PIXMAP_BIT,
+	GLX_BIND_TO_TEXTURE_TARGETS_EXT, GLX_TEXTURE_RECTANGLE_BIT_EXT,
+	GLX_BIND_TO_TEXTURE_RGBA_EXT, True,
+	GLX_DOUBLEBUFFER, False,
+	GLX_Y_INVERTED_EXT, GLX_DONT_CARE,
+	None
+};*/
+static int pixmap_attr[] = {
+	GLX_TEXTURE_FORMAT_EXT, GLX_TEXTURE_FORMAT_RGBA_EXT, 
+	GLX_TEXTURE_TARGET_EXT, GLX_TEXTURE_RECTANGLE_BIT_EXT,
+	None
+};
+#endif // _UWM_H_
